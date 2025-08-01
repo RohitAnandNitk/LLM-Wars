423a6d532a4404694c8655d9c4121c1c20344d17
RohitAnandNitk <sharmarohit64748@gmail.com>
2025-08-01 13:15:53 +0530

    socket url updated

--- frontend/src/SocketIO/socketio.jsx
+++ frontend/src/SocketIO/socketio.jsx
@@ -1,6 +1,11 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+// const socket = io("http://localhost:5000", {
+//   transports: ["websocket"], // Avoid polling issues
+//   withCredentials: true,
+// });
+
+const socket = io("https://llm-wars.onrender.com", {
   transports: ["websocket"], // Avoid polling issues
   withCredentials: true,
 });
